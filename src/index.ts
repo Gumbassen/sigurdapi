642ac8554bac7b46f29f184d394fbc53ef7bde18
kasp315b <gumbas.2601@gmail.com>
2023-05-23 14:29:39 +0200

    Moved source to src folder

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,11 @@ const port         = process.env.PORT
 
 app.get('/', (req: Request, res: Response) =>
 {
-    logger.info('Hello world was visited.')
+    logger.http('Hello world was visited.')
     res.send('Hello World!')
 })
 
 app.listen(port, () =>
 {
-    logger.info(`[SERVER] Listening on port ${port}`)
+    logger.info(`⚡ [SERVER] Listening on port ${port}`)
 })
