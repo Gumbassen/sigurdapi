adff1df6e355ba5b2484c4cdf5063c54256920c7
kasp315b <gumbas.2601@gmail.com>
2023-05-25 12:32:00 +0200

    Finished setting things up for adding an SQL connection

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import dotenv from 'dotenv'
 import swagger from 'swagger-ui-dist'
 import fsrecursivesearch from './utils/fsrecursivesearch'
 import authmw from './middlewares/auth'
+import mapiterator from './utils/mapiterator'
 dotenv.config()
 
 // Changes the working directory to the where index.ts/js is.
@@ -17,54 +18,64 @@ process.chdir(__dirname)
 const app  = express()
 const port = Number.parseInt(process.env.PORT ?? '6969')
 
-
-
-
+// Initialize middlewares
 app.use(express.json())
 app.use(authmw({
-    insecureFilter: request => [ '/auth/authenticate', '/auth/refresh' ].includes(request.url),
+    insecureFilter: request =>
+    {
+        if([ '/auth/authenticate', '/auth/refresh', '/' ].includes(request.url))
+            return true
+
+        if(request.url.startsWith('/swagger'))
+            return true
+
+        if(request.url.startsWith('/static'))
+            return true
+
+        return false
+    },
 }))
 
-
-
-
+// This shit is just for debugging
+// Dont mind
 app.get('/', (req, res) =>
 {
-    // This shit is just for debugging
-    // Dont mind
     log.http('Hello world was visited.')
     res.send('Hello World!')
 })
 
-
+// Swagger routes
 app.use('/swagger', express.static(swagger.absolutePath()))
 app.use('/static', express.static('./../static'))
 
 
-// Autoload routes from the routes folder
-for(const path of fsrecursivesearch('./routes', ({ name }) => name === 'endpoint.js'))
-{
-    const prefixRx       = /^\.\/routes((?:\/\w+)+)\/endpoint.js$/g
-    const prefixRxResult = prefixRx.exec(path)
-    if(prefixRxResult === null) continue
-    const prefix = prefixRxResult[1]
+Promise.all([
+    // Autoloads routes
+    Promise.all(mapiterator(fsrecursivesearch('./routes', ({ name }) => name === 'endpoint.js'), path =>
+    {
+        const prefixRx       = /^\.\/routes((?:\/\w+)+)\/endpoint.js$/g
+        const prefixRxResult = prefixRx.exec(path)
+        if(prefixRxResult === null) return Promise.reject(`Invalid route path: ${path}`)
+        const prefix = prefixRxResult[1]
 
-    log.verbose(`Autoloading route: "${prefix}" from "${path}"`)
-
-    import(path)
-        .then(module =>
+        return import(path).then(module =>
         {
+            log.verbose(`Autoloading route: "${prefix}" from "${path}"`)
             app.use(prefix, module.default)
-        }).catch(error =>
-        {
+        }).catch(error => ({ error, path }))
+    })).catch(errors =>
+    {
+        for(const { error, path } of errors)
             log.error(`Failed to import route endpoint file "${path}": "${error}"`)
-        })
-}
-
-
-
-// Start the server
-app.listen(port, () =>
+    }),
+]).then(() =>
 {
-    log.info(`⚡ [SERVER] Listening on port ${port}`)
+    // Start the server
+    app.listen(port, () =>
+    {
+        log.info(`⚡ [SERVER] Listening on port ${port}`)
+    })
+}).catch(errors =>
+{
+    log.error(`Failed to start...\n\t${errors.map(String).join('\n\t')}`)
 })
